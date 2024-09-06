466f664a474ce8be95f22f31d4d53f42ffe3f7e2
Violineering <alstonh021128@1utar.my>
2024-09-06 12:54:40 +0800

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,19 +1,17 @@
+<link rel="stylesheet" href="css/styles.css">
 <footer>
     <div style="text-align: center; padding: 20px; font-size: 14px; color: #555;">
         <p>
             Any enquries: 
-            <a href="" style="color: #0066cc; text-decoration: none;">Contact Us</a> 
-            or 
-            call +60 3-1234 5678.
+            <a href="/pineApple/Contact.php" style="color: #0066cc; text-decoration: none;">Contact Us</a> 
         </p>
         <hr style="border: 0; border-top: 1px solid #ccc;">
         <p style="margin: 10px 0;">
             &copy; 2024 pineApple Inc. All rights reserved. 
-            <a href="" style="color: #0066cc; text-decoration: none;">Privacy Policy</a> | 
-            <a href="" style="color: #0066cc; text-decoration: none;">Terms of Use</a> | 
-            <a href="" style="color: #0066cc; text-decoration: none;">Sales and Refunds</a> | 
-            <a href="" style="color: #0066cc; text-decoration: none;">Legal</a> | 
-            <a href="" style="color: #0066cc; text-decoration: none;">Site Map</a>
+            <a href="tel:+60123456789"><i class="fa fa-phone"></i> : +60 3-1234 5678</a> | 
+            <a href="mailto:pineApple@gmail.com"><i class="fa fa-envelope"></i> : pineApple@gmail.com</a> | 
+            <a href="https://www.facebook.com/pineApple"><i class="fa fa-facebook"></i> : pineApple</a> | 
+            <a href="https://www.instagram.com/pineApple"><i class="fa fa-instagram"></i> : pineApple</a> | 
         </p>
         <p style="color: #777;">Malaysia</p>
     </div>
