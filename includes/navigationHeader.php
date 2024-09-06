ee6ad661252e75bf3bd1eca0db07a8ba386529bc
Violineering <alstonh021128@1utar.my>
2024-09-06 14:01:19 +0800

    Update navigationHeader.php

--- includes/navigationHeader.php
+++ includes/navigationHeader.php
@@ -1,4 +1,6 @@
 <link rel="stylesheet" href="../css/NavigationHeader.css">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <header class="navbar clearfix">
         <div class="navbar-container">
             <a href="index.php" class="logo">
